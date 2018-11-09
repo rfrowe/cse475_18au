714f0654ab4f99d71aca465648cece32a5cbbd47
Kheng Wei Ang <khengwei118@gmail.com>
2018-11-08 16:23:40 -0800

    implemented transition, math to be calculated

--- main/State.cpp
+++ main/State.cpp
@@ -29,11 +29,70 @@ bool State::rxStartle(uint8_t len, uint8_t* payload) {
   // TODO: implement
 }
 
-void State::txStartle(uint8_t strength, uint8_t id) {
+void State::txStartle(uint8_t strength, uint8_t id) {  
   // TODO: implement
 }
 
+int[] State::calProb() {
+  // TODO: Implement math to replace this
+  int probabilities [7] = [5, 7, 12, 3, 2, 9, 23];
+  return probabilities;
+}
+
+State* State::getNewState(int stateNum) {
+  switch(stateNum) {
+    // TODO: return states to be filled
+    case 1:
+      // return wait state
+      break;
+    case 2:
+      // return active1
+      break;
+    case 3:
+      // return active2
+      break;
+    case 4:
+      // return active3
+      break;
+    case 5:
+      // return ambient1
+      break;
+    case 6:
+      // return ambient2
+      break;
+    case 7:
+      // return ambient3
+      break;
+  }
+}
+
 State* State::transition() {
+  int probabilities [7] = calProb();
+  int cumProbabilities [7];
+  int stateNum = 0;
+  cumProbabilities = probabilities;
+  for (int i = 1; i < probabilities.length(); i++) {
+    cumProbabilities[i] = cumProbabilities[i - 1] + probabilities[i];
+  }
+  int num = rand(0,cumProbabilities[6]);
+  for (int i = cumProbilities.length() - 1; i >= 0; i--) {
+    if (num <= cumProbabilities[i]) {
+      stateNum = i;
+    }
+  }
+  return State::getNewState(stateNum);
+//  // boundary numbers
+//  for (int i = 0; i < cumProbabilities.length(); i++) {
+//    if (num == cumProbabilities[i]) {
+//      stateNum = i;
+//    }
+//  }
+//  // otherwise round up to the next index
+//  for (int i = 0; i < cumProbabilities.length() - 1; i++) {
+//    if (num >= cumProbabilities[i] && num <= cumProbabilities[i+1]) {
+//      stateNum = i+1;
+//    }
+//  }
   // TODO: implement
 }
 
