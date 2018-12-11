cb93093bc058dfcdde12aaf6d376dfe57800b7e5
Ryan Rowe <ryanf.rowe@gmail.com>
2018-12-10 19:39:09 -0800

    Add sounds and lights to startle

--- main/Startle.cpp
+++ main/Startle.cpp
@@ -1,12 +1,16 @@
 #include "Startle.h"
+#include "Midi.h"
+#include "Neopixel.h"
 
 constexpr uint8_t Startle::_localWeights[];
 
 uint8_t Startle::getNumRepeats() {
- return rand() % 4 + 1; // 1 - 4
+  return rand() % 4 + 1; // 1 - 4
 }
 
 void Startle::loop(uint32_t dt) {
+  Midi::setSound(0x27, true);
+  Neopixel::setLight(0x03);
 }
 
 const uint8_t* Startle::getLocalWeights() {
