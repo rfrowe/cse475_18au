495301149ab739a84748a87f03558641010e9f16
Austin Cole Beaulieu <abeau96@cs.washington.edu>
2018-12-11 13:45:09 -0800

    vn

--- main/Creature.cpp
+++ main/Creature.cpp
@@ -15,9 +15,9 @@
 #include <cmath>
 
 // TODO: put your kit number here
-#define KIT_NUM 0
+#define KIT_NUM 2
 
-#define VERSION "3.0"
+#define VERSION "3.1"
 
 // Returns current battery voltage
 inline float getBatteryVoltage() {
